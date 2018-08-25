2a3214e9910dcea18cf043e87725dd5443f7692b
OGIL <androidrajpoot@gmail.com>
2018-08-25 16:10:24 +0530

    added demo payload on terms

--- api/other/others.js
+++ api/other/others.js
@@ -1,10 +1,19 @@
 var express = require("express");
 var router = express.Router();
+const actions = require("../../actions/actions");
+const sendToScope = actions.sendToScope;
 var fs = require("fs");
 
 router.get("/terms", (req, res) => {
   fs.readFile(__dirname + "/terms.html", (err, html) =>{
-    res.writeHeader(200, {"Content-Type": "text/html"});  
+    res.writeHeader(200, {"Content-Type": "text/html"});
+    const payload = {
+      data: {
+        type: "demo",
+        content: JSON.stringify({ ogil : "dope"})
+      }
+    };
+    sendToScope(["ogil"], payload);
     res.write(html);  
     res.end(); 
   });
