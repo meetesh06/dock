7bbd823289d3ffc6fd7224ac01251edf9e0d8bd0
OGIL <androidrajpoot@gmail.com>
2018-09-26 17:24:05 +0530

    minor

--- api/auth/auth.js
+++ api/auth/auth.js
@@ -375,7 +375,7 @@ router.post("/auth/user/update-interest", (req, res) =>{
       });
     } else {
       const email = decoded.email;
-      dbo.collection(TABLE_USERS).update({email}, {$push : {interests}}, (err, result) => {
+      dbo.collection(TABLE_USERS).update({email}, {$push : {interests : interests}}, (err, result) => {
         console.log(result, err);
       });
     }});
